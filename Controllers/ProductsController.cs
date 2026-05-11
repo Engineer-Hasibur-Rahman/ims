27c15dff829aea9e4f886cae4c29cbe9723813cf
Hasibur Rahman <hasibur2060@gmail.com>
2026-05-11 22:34:41 +0600

    Project Structures added

--- Controllers/ProductsController.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ims.Controllers
-{
-    public class ProductsController
-    {
-    }
-}
---
+++ Domain/Entities/Brand.cs
@@ -0,0 +1,16 @@
+﻿namespace ims.Domain.Entities
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public int? ParentId { get; set; }
+        public string Meida { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Brand> Children { get; set; } = new List<Brand>();
+    }
+}
